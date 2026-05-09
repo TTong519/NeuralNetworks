31ed1d5b299284a6633eb6ac0a8bbf905c28d666
Tyler.Tong <Tyler.Tong@GMR-118-2-3>
2026-05-08 19:04:05 -0700

    Add project files.

---
+++ CommonLib/ConverterExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLib
+{
+    public static class ConverterExtensions
+    {
+        public static List<double> ToDoubleList(this string str)
+        {
+            var parts = str.ToCharArray();
+            var result = new List<double>();
+            foreach(var part in parts)
+            {
+                result.Add(part);
+            }
+            return result;
+        }
+    }
+}
---
+++ HillClimberDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using CommonLib;
+namespace HillClimberDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Random rand = new();
+                Console.WriteLine("Enter a initial string:");
+                string initial = Console.ReadLine();
+                Console.WriteLine("Enter a target string:");
+                string target = Console.ReadLine();
+                var initialList = initial.ToDoubleList();
+                var targetList = target.ToDoubleList();
+                double mae = initialList.MAE(targetList);
+                Console.WriteLine($"Current: {initial} -> Target: {target} | MAE: {mae}");
+                while (mae > 0)
+                {
+                    List<double> lastState = new(initialList);
+                    Console.Write("Current: ");
+                    initialList[rand.Next(initialList.Count)] += Math.Round(rand.NextDouble() * 2 - 1);
+                    foreach(char c in initialList)
+                    {
+                        Console.Write(c);
+                    }
+                    Console.WriteLine($" -> {target} | MAE: {mae}");
+                    if(initialList.MAE(targetList) < mae)
+                    {
+                        Console.WriteLine($"Improvement found! Last MAE: {mae} -> New MAE: {initialList.MAE(targetList)}");
+                        mae = initialList.MAE(targetList);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"No improvement. Last MAE: {mae} < New MAE: {initialList.MAE(targetList)}");
+                        initialList = lastState;
+                    }
+                }
+                Console.WriteLine($"Target string '{target}' reached from initial string '{initial}'!");
+            }
+        }
+    }
+}
