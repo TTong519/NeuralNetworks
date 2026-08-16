f58762196c337c1a118d2fe5b15b4fa0cf17049c
Tyler <tylertong519@gmail.com>
2026-08-15 21:57:31 -0700

    work on monte carlo logic

--- CommonLib/MonteCarlo.cs
+++ CommonLib/MonteCarlo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,34 @@ namespace CommonLib
 {
     class MonteCarloTree
     {
-	    public static (int, T) MonteCarlo<T>(T state, bool isMax, int simulations = 1000) where T : IMonteCarloGameState<T>
+	    public static (int, T) MonteCarlo<T>(T state, bool isMax, int iterations = 1600) where T : IMonteCarloGameState<T>
 	    {
 		    
+	    }
+
+	    private static T Select<T>(T root) where T : IMonteCarloGameState<T>
+	    {
+		    T current = root;
+		    while (current.Children.Count > 0)
+		    {
+			    double bestUCTValue = double.MinValue;
+			    T  bestChild = null;
+			    foreach (var child in current.Children)
+			    {
+				    double UCT = (child.Count / child.Value) + 1.5 * (System.Math.Sqrt(System.Math.Log(root.Value)) / child.Value);
+				    if (UCT > bestUCTValue)
+				    {
+					    bestUCTValue = UCT;
+					    bestChild = child;
+				    }
+ 			    }
+			    if(bestChild != null)
+			    {
+				    break;
+			    }
+			    current = bestChild;
+		    }
+		    return current;
 	    }
     }
 }
